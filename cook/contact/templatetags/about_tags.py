41c13037e5efe49b03f03deab79dceeb66a4b3dd
Lev <lev.yakovenko11@gmail.com>
2023-04-23 17:33:46 +0700

    admin settings in blog and contact have been changed. Comments too

--- cook/contact/templatetags/about_tags.py
+++
@@ -1,9 +0,0 @@
-from contact.models import AboutModel
-from django.template.defaulttags import register
-
-
-@register.inclusion_tag('include_templates/templates_for_tags/short_about_info.html')
-def get_short_about_info():
-    """ Для отображения информации в left-menu """
-    short_about_info = AboutModel.objects.all()
-    return {'last_about_info': short_about_info}
